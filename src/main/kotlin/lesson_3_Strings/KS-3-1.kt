72feaef2454a266d8d0c76de5819dd67943e60d1
trm600 <trm600@yahoo.com>
2026-03-31 15:12:40 +0300

    KS-3-1 add greeting in the application

---
+++ src/main/kotlin/lesson_3_Strings/KS-3-1.kt
@@ -0,0 +1,11 @@
+﻿package org.example.lesson_3_Strings
+
+const val DAY_HI = "Добрый день! "
+const val EVENING_HI = "Добрый вечер! "
+fun main() {
+    val userName = "Порфирий"
+    var helloSay = DAY_HI + userName
+    println(helloSay)
+    helloSay = EVENING_HI + userName
+    println(helloSay)
+}
