7f83f130b8b891ff206943b26bbe31dd25fbedf6
trm600 <trm600@yahoo.com>
2026-03-31 16:46:37 +0300

    KS-3-1 fix review comments

--- src/main/kotlin/lesson_3_Strings/KS-3-1.kt
+++ src/main/kotlin/lesson_3_Strings/KS-3-1.kt
@@ -1,12 +1,11 @@
 ﻿package org.example.lesson_3_Strings
 
-const val DAY_HI = "Добрый день"
-const val EVENING_HI = "Добрый вечер"
-
 fun main() {
+    val dayHi = "Добрый день"
+    val eveningHi = "Добрый вечер"
     val userName = "Порфирий"
-    var helloSay = "$DAY_HI! $userName"
+    var helloSay = "$dayHi! $userName"
     println(helloSay)
-    helloSay = "$EVENING_HI! $userName"
+    helloSay = "$eveningHi! $userName"
     println(helloSay)
 }
