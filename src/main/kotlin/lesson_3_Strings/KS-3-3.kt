860913d9eb77ed8d7e35b61dc2895b2e52992e7a
trm600 <trm600@yahoo.com>
2026-04-01 23:05:38 +0300

    KS-3-3 multiplication table

---
+++ src/main/kotlin/lesson_3_Strings/KS-3-3.kt
@@ -0,0 +1,12 @@
+﻿package org.example.lesson_3_Strings
+
+fun main() {
+    val factor = readln().toByte()
+
+    if (factor <= 9) {
+        for (i in 1..9) {
+            println("$factor x $i = ${factor * i}")
+        }
+
+    }
+}
