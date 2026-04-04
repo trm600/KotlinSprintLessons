a25de510f4827fb60357a7e559e27905bd6bdfbd
trm600 <trm600@yahoo.com>
2026-04-04 11:37:22 +0300

    KS-4-1 wrote the code for the task

--- src/main/kotlin/lesson_4_comparison_operators/KS-4-1.kt
+++ src/main/kotlin/lesson_4_comparison_operators/KS-4-1.kt
@@ -1,2 +1,10 @@
 ﻿package org.example.lesson_4_comparison_operators
 
+const val TOTAL_TABLES: Byte = 13
+
+fun main() {
+    val bookedToday: Byte = 13
+    val bookedTomorrow: Byte = 9
+
+    println("Доступность столиков на сегодня:${bookedToday < TOTAL_TABLES}\nДоступность столиков на завтра:${bookedTomorrow < TOTAL_TABLES}")
+}
