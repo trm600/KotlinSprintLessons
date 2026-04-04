88fcad05f47285f29e4eb20992d2aa2e97d19977
trm600 <trm600@yahoo.com>
2026-04-04 15:18:28 +0300

    KS-4-3 fix review comments

--- src/main/kotlin/lesson_4_comparison_operators/KS-4-3.kt
+++ src/main/kotlin/lesson_4_comparison_operators/KS-4-3.kt
@@ -1,11 +1,17 @@
 ﻿package org.example.lesson_4_comparison_operators
 
+const val FAVORABLE_AIR_HUMIDITY = 20
+const val UNFAVORABLE_SEASON = "зима"
+
 fun main() {
     val isSunnyWeather = true
     val isAwningOpen = true
     val airHumidity = 20
     val timeOfYear = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? " +
-    "${isSunnyWeather && isAwningOpen && airHumidity == 20 && !(timeOfYear.equals("зима", ignoreCase = true))}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? " + "${
+            isSunnyWeather && isAwningOpen && airHumidity == FAVORABLE_AIR_HUMIDITY && timeOfYear != UNFAVORABLE_SEASON
+        }"
+    )
 }
