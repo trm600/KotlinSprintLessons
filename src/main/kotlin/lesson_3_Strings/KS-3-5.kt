49e5da9740b0db687cd37ec3962bde4484617992
trm600 <trm600@yahoo.com>
2026-04-04 10:42:30 +0300

    KS-3-5 wrote the task code

--- src/main/kotlin/lesson_3_Strings/KS-3-5.kt
+++ src/main/kotlin/lesson_3_Strings/KS-3-5.kt
@@ -1,5 +1,13 @@
 package org.example.lesson_3_Strings
 
 fun main() {
+    val currentMove = "D2-D4;0"
+    val listOfParameter = currentMove.split("-", ";")
+    val moveFrom = listOfParameter[0]
+    val moveWhere = listOfParameter[1]
+    val moveNumber = listOfParameter[2]
 
+    println(moveFrom)
+    println(moveWhere)
+    println(moveNumber)
 }
