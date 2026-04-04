f3b71744c178d450bd1f1d69c95d75a34eb67c6c
trm600 <trm600@yahoo.com>
2026-04-04 17:41:51 +0300

    KS-4-4 wrote the code for the exercise

---
+++ src/main/kotlin/lesson_4_comparison_operators/KS-4-4.kt
@@ -0,0 +1,14 @@
+﻿package org.example.lesson_4_comparison_operators
+
+fun main() {
+    var tranningDay = 6
+
+    println(
+        """
+        Упражнения для рук: ${tranningDay % 2 == 1}
+        Упражнения для ног: ${tranningDay % 2 == 0}
+        Упражнения для спины: ${tranningDay % 2 == 0}
+        Упражнения для пресса: ${tranningDay % 2 == 1}
+        """.trimIndent()
+    )
+}
