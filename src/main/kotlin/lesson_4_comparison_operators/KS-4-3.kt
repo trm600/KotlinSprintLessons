c0c7147861149f9724f8dd163af5073cf33351b0
trm600 <trm600@yahoo.com>
2026-04-04 15:03:49 +0300

    KS-4-3 wrote the code for the task

---
+++ src/main/kotlin/lesson_4_comparison_operators/KS-4-3.kt
@@ -0,0 +1,11 @@
+﻿package org.example.lesson_4_comparison_operators
+
+fun main() {
+    val isSunnyWeather = true
+    val isAwningOpen = true
+    val airHumidity = 20
+    val timeOfYear = "зима"
+
+    println("Благоприятные ли условия сейчас для роста бобовых? " +
+    "${isSunnyWeather && isAwningOpen && airHumidity == 20 && !(timeOfYear.equals("зима", ignoreCase = true))}")
+}
