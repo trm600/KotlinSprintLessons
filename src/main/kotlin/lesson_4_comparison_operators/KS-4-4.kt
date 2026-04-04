fc238f7109232d18c5deb8c46f9e169061a10474
trm600 <trm600@yahoo.com>
2026-04-04 17:51:52 +0300

    KS-4-4 rename var trainingDay

--- src/main/kotlin/lesson_4_comparison_operators/KS-4-4.kt
+++ src/main/kotlin/lesson_4_comparison_operators/KS-4-4.kt
@@ -1,14 +1,14 @@
 ﻿package org.example.lesson_4_comparison_operators
 
 fun main() {
-    var tranningDay = 6
+    var trainingDay = 6
 
     println(
         """
-        Упражнения для рук: ${tranningDay % 2 == 1}
-        Упражнения для ног: ${tranningDay % 2 == 0}
-        Упражнения для спины: ${tranningDay % 2 == 0}
-        Упражнения для пресса: ${tranningDay % 2 == 1}
+        Упражнения для рук: ${trainingDay % 2 == 1}
+        Упражнения для ног: ${trainingDay % 2 == 0}
+        Упражнения для спины: ${trainingDay % 2 == 0}
+        Упражнения для пресса: ${trainingDay % 2 == 1}
         """.trimIndent()
     )
 }
