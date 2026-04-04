ee7ba04f2004c2d0d7bead2eb05ed329dcea1675
trm600 <trm600@yahoo.com>
2026-04-04 11:46:22 +0300

    KS-4-1 fix review comments

--- src/main/kotlin/lesson_4_comparison_operators/KS-4-1.kt
+++ src/main/kotlin/lesson_4_comparison_operators/KS-4-1.kt
@@ -1,10 +1,1 @@
-﻿package org.example.lesson_4_comparison_operators
-
-const val TOTAL_TABLES: Byte = 13
-
-fun main() {
-    val bookedToday: Byte = 13
-    val bookedTomorrow: Byte = 9
-
-    println("[Доступность столиков на сегодня: ${bookedToday < TOTAL_TABLES}],\n[Доступность столиков на завтра: ${bookedTomorrow < TOTAL_TABLES}]")
-}
+﻿
