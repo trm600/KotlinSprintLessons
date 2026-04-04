8fdfff5d1b01d98f1ca2f8c3f80c2237cc6b72b5
trm600 <trm600@yahoo.com>
2026-04-04 14:08:12 +0300

    KS-4-2 fix review comments

--- src/main/kotlin/lesson_4_comparison_operators/KS-4-2.kt
+++ src/main/kotlin/lesson_4_comparison_operators/KS-4-2.kt
@@ -9,8 +9,11 @@ fun main() {
     var weight = 20
     var capacity = 80
 
-    println("Груз с весом $weight кг и объемом $capacity л соответствует категории 'Average': ${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && capacity < MAX_CAPACITY}")
+    println("Груз с весом $weight кг и объемом $capacity л " +
+            "соответствует категории 'Average': " +
+            "${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && capacity < MAX_CAPACITY}")
     weight = 50
     capacity = 100
-    println("Груз с весом $weight кг и объемом $capacity л соответствует категории 'Average': ${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && capacity < MAX_CAPACITY}")
-}
+    println("Груз с весом $weight кг и объемом $capacity л " +
+            "соответствует категории 'Average': " +
+            "${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && capacity < MAX_CAPACITY}")}
