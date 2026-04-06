f2d393087d8851b197728192ea67eed9940a2a69
trm600 <trm600@yahoo.com>
2026-04-06 17:28:19 +0300

    KS-4-5 fix review comments

--- src/main/kotlin/lesson_4_comparison_operators/KS-4-5.kt
+++ src/main/kotlin/lesson_4_comparison_operators/KS-4-5.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_4_comparison_operators
 
+const val MINIMUM_CREW = 55
+const val MAXIMUM_CREW = 70
+const val MINIMUM_PROVISIONS = 50
+
+
 fun main() {
     val isBodyDamaged: Boolean
     val numberOfCrew: Int
@@ -7,20 +12,32 @@ fun main() {
     val isGoodWeather: Boolean
     val canVoyage: Boolean
     val canVoyageAlt: Boolean
+    val isMinorDamage: Boolean
 
     print("Корпус повреждён?: ")
     isBodyDamaged = readln().toBoolean()
+    if (isBodyDamaged) {
+        print("Повреждения незначительные?: ")
+        isMinorDamage = readln().toBoolean()
+    } else {
+        isMinorDamage = false
+    }
     print("Число экипажа: ")
     numberOfCrew = readln().toInt()
     print("Число ящиков с провизией: ")
     provisionsBoxes = readln().toInt()
     print("Погода хорошая?: ")
     isGoodWeather = readln().toBoolean()
-    canVoyage = !isBodyDamaged && numberOfCrew in 55..70 && provisionsBoxes > 50
-    canVoyageAlt = numberOfCrew == 70 && isGoodWeather && provisionsBoxes >= 50
+    canVoyage = !isBodyDamaged &&
+            numberOfCrew in MINIMUM_CREW..MAXIMUM_CREW &&
+            provisionsBoxes > MINIMUM_PROVISIONS
+    canVoyageAlt = numberOfCrew == MAXIMUM_CREW &&
+            isGoodWeather &&
+            provisionsBoxes >= MINIMUM_PROVISIONS &&
+            (!isBodyDamaged || isBodyDamaged && isMinorDamage)
     print("Корабль может отплыть?: ")
-            when (canVoyage || canVoyageAlt) {
-                true -> print("Да")
-                false ->print("Нет")
-            }
+    when (canVoyage || canVoyageAlt) {
+        true -> print("Да")
+        false -> print("Нет")
+    }
 }
