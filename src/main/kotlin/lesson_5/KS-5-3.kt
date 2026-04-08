3b208935dc4d100d2298a3b65d0bdd16c5541a4c
trm600 <trm600@yahoo.com>
2026-04-08 16:02:28 +0300

    KS-5-3 added code to the task

---
+++ src/main/kotlin/lesson_5/KS-5-3.kt
@@ -0,0 +1,24 @@
+﻿package org.example.lesson_5
+
+fun main() {
+
+    val winningNumber1 = 5
+    val winningNumber2 = 13
+
+    println("Введите два числа от 0 до 42:")
+    val guessedNumber1 = readln().toInt()
+    val guessedNumber2 = readln().toInt()
+
+    if ((guessedNumber1 == winningNumber1 || guessedNumber1 == winningNumber2) &&
+        (guessedNumber2 == winningNumber1 || guessedNumber2 == winningNumber2)
+    ) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (guessedNumber1 == winningNumber1 || guessedNumber1 == winningNumber2 ||
+        guessedNumber2 == winningNumber1 || guessedNumber2 == winningNumber2
+    ) {
+        println("Вы выиграли утешительный приз!")
+    } else {
+        println("Неудача!")
+    }
+    println("Выигрышная комбинация числа: $winningNumber1 и $winningNumber2")
+}
