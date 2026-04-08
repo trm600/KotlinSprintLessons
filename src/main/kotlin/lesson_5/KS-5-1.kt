a7a8dfbeb32d1cfe9b55b9d7315fcff871bc50bd
trm600 <trm600@yahoo.com>
2026-04-08 09:42:26 +0300

    KS-5-1 added code to the task

---
+++ src/main/kotlin/lesson_5/KS-5-1.kt
@@ -0,0 +1,14 @@
+﻿package org.example.lesson_5
+
+const val CORRECT_ANSWER = 4
+
+fun main() {
+
+    print("Докажи, что не бот, - 2 + 2 = ")
+    val answer = readln().toIntOrNull()
+    if (answer == CORRECT_ANSWER) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
+    }
+}
