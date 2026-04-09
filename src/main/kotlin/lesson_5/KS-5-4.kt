2b0a6412d0f8eebd9a65d2b50e5b1b048fe2ca42
trm600 <trm600@yahoo.com>
2026-04-09 20:38:22 +0300

    KS-5-4 added code to the task

---
+++ src/main/kotlin/lesson_5/KS-5-4.kt
@@ -0,0 +1,21 @@
+﻿package org.example.lesson_5
+
+const val USER = "Zaphod"
+const val PASS = "PanGalactic"
+
+fun main() {
+
+    print("Введите имя пользователя: ")
+    val userName = readln()
+    when (userName) {
+        USER -> {
+            print("Введите пароль: ")
+            val password = readln()
+            when (password) {
+               PASS -> println("Пользователь \"$userName\", вам разрешено " +
+                       "входить на борт корабля \"Heart of Gold\"")
+            }
+        }
+        else -> println("Пожалуйста зарегистрируйтесь")
+    }
+}
