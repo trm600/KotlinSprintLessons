8891c9b1e28912c9b2f1688097ef2abedeb4280d
trm600 <trm600@yahoo.com>
2026-04-10 20:54:05 +0300

    KS-5-5 fix review comments

--- src/main/kotlin/lesson_5/KS-5-5.kt
+++ src/main/kotlin/lesson_5/KS-5-5.kt
@@ -1,8 +1,8 @@
 package org.example.lesson_5
 
 const val NUMBER_OF_WINNING_NUMBERS = 3
-const val MIN = 0
-const val MAX = 42
+const val MIN_LOTTERY_NUMBER = 0
+const val MAX_LOTTERY_NUMBER = 42
 
 fun main() {
     val winningNumbers: MutableList<Int> = mutableListOf()
@@ -11,7 +11,7 @@ fun main() {
     val numberOfMatches: Int
 
     while (counter != NUMBER_OF_WINNING_NUMBERS) {
-        val number = (MIN..MAX).random()
+        val number = (MIN_LOTTERY_NUMBER..MAX_LOTTERY_NUMBER).random()
         if (!winningNumbers.contains(number)) {
             winningNumbers.add(number)
             ++counter
@@ -24,17 +24,18 @@ fun main() {
         if (!guessedNumbers.contains(number)) {
             guessedNumbers.add(number)
             ++counter
-        }
-        else {
+        } else {
             println("Это число уже было введено, введите другое число.")
         }
     }
     numberOfMatches = guessedNumbers.intersect(winningNumbers.toSet()).size
-    println(when (numberOfMatches) {
-        3 -> "Вы угадали все числа и выиграли джекпот!"
-        2 -> "Вы угадали два числа и получаете крупный приз!"
-        1 -> "Вы угадали одно число и получате утешительный приз!"
-        else -> "Вы не угадали ни одного числа."
-    })
+    println(
+        when (numberOfMatches) {
+            3 -> "Вы угадали все числа и выиграли джекпот!"
+            2 -> "Вы угадали два числа и получаете крупный приз!"
+            1 -> "Вы угадали одно число и получаете утешительный приз!"
+            else -> "Вы не угадали ни одного числа."
+        }
+    )
     println("Выигрышные числа: $winningNumbers")
 }
