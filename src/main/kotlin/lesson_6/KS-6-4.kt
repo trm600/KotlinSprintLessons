9171d55d04638ca5ff391df8567d223f1eecafc5
trm600 <trm600@yahoo.com>
2026-04-12 16:20:15 +0300

    KS-6-4 added code to the task

---
+++ src/main/kotlin/lesson_6/KS-6-4.kt
@@ -0,0 +1,26 @@
+﻿package org.example.lesson_6
+
+const val MINIMUM_NUMBER = 1
+const val MAXIMUM_NUMBER = 9
+const val NUMBER_OF_ATTEMPTS = 5
+
+fun main() {
+    var counter = 1
+    val bingo = (MINIMUM_NUMBER..MAXIMUM_NUMBER).random()
+
+    while (counter <= NUMBER_OF_ATTEMPTS) {
+        println("Введи число от $MINIMUM_NUMBER до $MAXIMUM_NUMBER: ")
+        val userNumber = readln().toInt()
+        when {
+            userNumber == bingo -> {
+                println("Это была великолепная игра!")
+                break
+            }
+            else -> {
+                println("Фиг вам, попыток осталось: ${NUMBER_OF_ATTEMPTS - counter} )")
+            }
+        }
+        counter++
+    }
+    println("Было загадано число $bingo")
+}
