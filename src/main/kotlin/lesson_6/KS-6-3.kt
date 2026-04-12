ed584b84c859aeb90a2e56bad6f7ce6808ac9238
trm600 <trm600@yahoo.com>
2026-04-12 15:38:19 +0300

    KS-6-3 added code to the task

---
+++ src/main/kotlin/lesson_6/KS-6-3.kt
@@ -0,0 +1,11 @@
+﻿package org.example.lesson_6
+
+fun main() {
+    print("Введите количество секунд таймера: ")
+    var numberOfSeconds = readln().toInt()
+    while (numberOfSeconds > 0) {
+        println("Осталось секунд: ${numberOfSeconds--}")
+        Thread.sleep(1000)
+    }
+    println("Время вышло.")
+}
