0ad1d6e838a20dc3265981ddbf3c21f163e21d7e
trm600 <trm600@yahoo.com>
2026-04-12 10:09:33 +0300

    KS-6-1 wrote the code for the task

---
+++ src/main/kotlin/lesson_6/KS-6-1.kt
@@ -0,0 +1,16 @@
+﻿package org.example.lesson_6
+
+fun main() {
+    print("Введите желаемый логин: ")
+    val login = readln()
+    print("Введите желаемый пароль: ")
+    val password = readln()
+
+    do {
+        println("Введите ваш логин: ")
+        val inputLogin = readln()
+        println("Введите ваш пароль: ")
+        val inputPassword = readln()
+    } while (inputLogin != login || inputPassword != password)
+    println("Авторизация прошла успешно!")
+}
