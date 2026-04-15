29a81703d476351a30eff875bd78349447506966
trm600 <trm600@yahoo.com>
2026-04-15 18:27:30 +0300

    KS-6-5 fix review comments

--- src/main/kotlin/lesson_6/KS-6-5.kt
+++ src/main/kotlin/lesson_6/KS-6-5.kt
@@ -4,17 +4,18 @@ const val HOW_MANY_ATTEMPTS = 3
 
 fun main() {
     var attempts = HOW_MANY_ATTEMPTS
+
     do {
         val term1 = (1..9).random()
         val term2 = (1..9).random()
-        val sum = term1 + term2
 
         println("Докажи, что не бот, какова сумма чисел: $term1 и $term2 ?")
         val answer = readln().toInt()
-        if (answer == sum) {
+        if (answer == term1 + term2) {
             println("Добро пожаловать!")
             return
         }
-    } while (--attempts > 0)
+        --attempts
+    } while (attempts > 0)
     println("Доступ запрещен")
 }
