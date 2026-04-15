01ce8fd53252d346c7eb60ce8fc0030fe7e17d92
trm600 <trm600@yahoo.com>
2026-04-15 18:10:30 +0300

    KS-6-5 fix review comments

--- src/main/kotlin/lesson_6/KS-6-5.kt
+++ src/main/kotlin/lesson_6/KS-6-5.kt
@@ -1,21 +1,20 @@
 package org.example.lesson_6
 
-const val NUMBER_OF_ATTEMPTS1 = 3
+const val HOW_MANY_ATTEMPTS = 3
 
 fun main() {
-    for (i in NUMBER_OF_ATTEMPTS1 downTo 1) {
+    var attempts = HOW_MANY_ATTEMPTS
+    do {
         val term1 = (1..9).random()
         val term2 = (1..9).random()
         val sum = term1 + term2
 
         println("Докажи, что не бот, какова сумма чисел: $term1 и $term2 ?")
         val answer = readln().toInt()
-        if (answer != sum) {
-            continue
-        } else {
+        if (answer == sum) {
             println("Добро пожаловать!")
             return
         }
-    }
+    } while (--attempts > 0)
     println("Доступ запрещен")
 }
