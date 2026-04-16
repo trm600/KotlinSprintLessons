804ec44091fb7f8f262ed87504b9d3cb734fadd0
trm600 <trm600@yahoo.com>
2026-04-16 17:25:28 +0300

    KS-7-1 fix review comments

--- src/main/kotlin/lesson_7/KS-7-1.kt
+++ src/main/kotlin/lesson_7/KS-7-1.kt
@@ -1,20 +1,15 @@
 ﻿package org.example.lesson_7
 
-import kotlin.random.Random
-
 const val PASSWORD_SIZE = 6
-const val LETTERS_MAKING_KIT = "abcdefghijklmnopqrstuvwxyz"
-const val NUMBERS_MAKING_KIT = "0123456789"
-
 
 fun main() {
     var pass = ""
 
     for (i in 1..PASSWORD_SIZE) {
         pass += if (i % 2 == 0) {
-            NUMBERS_MAKING_KIT[Random.nextInt(NUMBERS_MAKING_KIT.length)]
+            ('a'..'z').random()
         } else {
-            LETTERS_MAKING_KIT[Random.nextInt(LETTERS_MAKING_KIT.length)]
+            (0..9).random()
         }
     }
     println(pass)
