6c9a590eb539a4eea1d8be25617398e727c78eb5
trm600 <trm600@yahoo.com>
2026-04-17 20:27:46 +0300

    KS-7-2 added the code to the task

---
+++ src/main/kotlin/lesson_7/KS-7-2.kt
@@ -0,0 +1,14 @@
+﻿package org.example.lesson_7
+
+fun main() {
+    do {
+        val autorizationCode = (1000..9999).random()
+        println("Ваш код авторизации: $autorizationCode")
+        println("Введите ваш код авторизации: ")
+        val enteredCode = readln().toInt()
+    } while (enteredCode != autorizationCode)
+    println("Авторизация успешна! Приветствуем!")
+
+
+
+}
