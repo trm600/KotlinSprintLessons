49f0cf0d8364b9ff123fdbd76cb9f4af0b2472b7
trm600 <trm600@yahoo.com>
2026-04-17 21:05:21 +0300

    KS-7-3 added the code to the task

---
+++ src/main/kotlin/lesson_7/KS-7-3.kt
@@ -0,0 +1,9 @@
+﻿package org.example.lesson_7
+
+fun main() {
+    println("Введите число: ")
+    val userNumber = readln().toInt()
+    for (i in 2..userNumber step 2) {
+        print("$i ")
+    }
+}
