923f0b451197a6f3173177919ae14f2915e345a3
trm600 <trm600@yahoo.com>
2026-04-17 21:41:51 +0300

    KS-7-4 fix review comments

--- src/main/kotlin/lesson_7/KS-7-4.kt
+++ src/main/kotlin/lesson_7/KS-7-4.kt
@@ -1,7 +1,7 @@
 ﻿package org.example.lesson_7
 
 fun main() {
-    println("Введите колличество секунд: ")
+    println("Введите количество секунд: ")
     val numberOfSeconds = readln().toInt()
     for (i in numberOfSeconds downTo 1) {
         println("Времени осталось $i сек.")
