cd54c69ba46d6113bc201e87bee61ce0169e1e4d
trm600 <trm600@yahoo.com>
2026-04-17 21:07:26 +0300

    KS-7-3 fix review comments

--- src/main/kotlin/lesson_7/KS-7-3.kt
+++ src/main/kotlin/lesson_7/KS-7-3.kt
@@ -3,7 +3,7 @@
 fun main() {
     println("Введите число: ")
     val userNumber = readln().toInt()
-    for (i in 2..userNumber step 2) {
+    for (i in 0..userNumber step 2) {
         print("$i ")
     }
 }
