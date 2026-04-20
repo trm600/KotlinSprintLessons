82d3690e778578fdd7c615b9bb4a68309048fc99
trm600 <trm600@yahoo.com>
2026-04-20 11:56:37 +0300

    KS-8-4 added the code to the task

---
+++ src/main/kotlin/lesson_8/KS-8-4.kt
@@ -0,0 +1,21 @@
+﻿package org.example.lesson_8
+
+fun main() {
+    val ingredientsList = arrayOf("селёдка", "лук", "картофель", "морковь", "яйцо", "свекла", "майонез")
+
+    println("Список ингредиентов: ${formatString(ingredientsList)}")
+    println("Введите название заменяемого ингредиента: ")
+    val ingredient = readln().lowercase()
+    if (ingredientsList.contains(ingredient)) {
+        println("Введите ингредиент для замены: ")
+        val newIngredient = readln().lowercase()
+        ingredientsList[ingredientsList.indexOf(ingredient)] = newIngredient
+        println("Готово! Вы сохранили следующий список: ${formatString(ingredientsList)}")
+    } else {
+        println("Такого ингредиента в рецепте нет")
+    }
+}
+
+fun formatString(list: Array<String>): String {
+    return "[${list.joinToString("_")}]"
+}
