ee404a6f4e0cbe638969adc3715c411c63d111ae
trm600 <trm600@yahoo.com>
2026-04-20 13:42:47 +0300

    KS-8-1 fix review comments

--- src/main/kotlin/lesson_8/KS-8-1.kt
+++ src/main/kotlin/lesson_8/KS-8-1.kt
@@ -3,5 +3,5 @@ package org.example.lesson_8
 fun main() {
     val viewsPerWeek = arrayOf(100, 200, 50, 400, 367, 456, 789)
 
-    println(viewsPerWeek.sum())
+    println("Количество просмотров рекламы за неделю: ${viewsPerWeek.sum()}")
 }
