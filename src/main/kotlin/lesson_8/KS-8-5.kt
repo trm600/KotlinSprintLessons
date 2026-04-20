7d5fb9ab18a7c36f64b39890b6a458be85852765
trm600 <trm600@yahoo.com>
2026-04-20 14:26:51 +0300

    KS-8-5 added the code to the task

---
+++ src/main/kotlin/lesson_8/KS-8-5.kt
@@ -0,0 +1,9 @@
+﻿package org.example.lesson_8
+
+fun main() {
+    println("Укажите планируемое количество ингредиентов: ")
+    val numberOfIngredients = readln().toInt()
+    println("Введите названия ингредиентов: ")
+    val arrayOfIngredients = Array(numberOfIngredients) { readln().lowercase() }
+    println("Список ингредиентов: ${arrayOfIngredients.joinToString(", ")}")
+}
