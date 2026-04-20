45eef248ec5c869997605b073e6c99ed508b0abd
trm600 <trm600@yahoo.com>
2026-04-20 16:49:33 +0300

    KS-9-2 added the code to the task

---
+++ src/main/kotlin/lesson_9/KS-9-2.kt
@@ -0,0 +1,18 @@
+﻿package org.example.lesson_9
+
+fun main() {
+    val listOfIngredients = mutableListOf("Селёдка", "Лук", "Картофель")
+
+    println("В рецепте есть базовые ингредиенты: ${getString(listOfIngredients)}")
+    println("Желаете добавить ещё?")
+    val userResponse = readln()
+    if (userResponse.equals("да", ignoreCase = true) ) {
+        println("Какой ингредиент вы хотите добавить?")
+        listOfIngredients.add(readln().replaceFirstChar { it.uppercase() })
+        println("Теперь в рецепте есть следующие ингредиенты: ${getString(listOfIngredients)}")
+    } else {
+        return
+    }
+}
+
+fun getString(list: List<String>) = list.joinToString(", ")
