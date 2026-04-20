03ce18746c85f9d9d9817ede4aa1499dba5f5fd2
trm600 <trm600@yahoo.com>
2026-04-20 16:15:07 +0300

    KS-9-1 added the code to the task

---
+++ src/main/kotlin/lesson_9/KS-9-1.kt
@@ -0,0 +1,8 @@
+﻿package org.example.lesson_9
+
+fun main() {
+    val ingredientsList = listOf("селёдка", "лук", "картофель", "морковь", "яйцо", "свекла", "майонез")
+
+    println("В рецепте есть следующие ингредиенты: [${ingredientsList.joinToString("_")}]")
+    ingredientsList.forEach { println(it) }
+}
