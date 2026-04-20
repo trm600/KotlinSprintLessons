9caa6a8681b739d256b52e802374170a4d87862d
trm600 <trm600@yahoo.com>
2026-04-20 16:54:33 +0300

    KS-9-2 removed else from the if condition

--- src/main/kotlin/lesson_9/KS-9-2.kt
+++ src/main/kotlin/lesson_9/KS-9-2.kt
@@ -6,12 +6,10 @@ fun main() {
     println("В рецепте есть базовые ингредиенты: ${getString(listOfIngredients)}")
     println("Желаете добавить ещё?")
     val userResponse = readln()
-    if (userResponse.equals("да", ignoreCase = true) ) {
+    if (userResponse.equals("да", ignoreCase = true)) {
         println("Какой ингредиент вы хотите добавить?")
         listOfIngredients.add(readln().replaceFirstChar { it.uppercase() })
         println("Теперь в рецепте есть следующие ингредиенты: ${getString(listOfIngredients)}")
-    } else {
-        return
     }
 }
 
