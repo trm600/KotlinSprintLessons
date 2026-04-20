2d626a39d433c2302a644f842d9dd58761673a7f
trm600 <trm600@yahoo.com>
2026-04-20 08:44:13 +0300

    KS-8-2 added the code to the task

---
+++ src/main/kotlin/lesson_8/KS-8-2.kt
@@ -0,0 +1,15 @@
+﻿package org.example.lesson_8
+
+fun main() {
+    val ingredientsList = arrayOf("Селёдка", "Лук", "Картофель", "Морковь", "Яйцо", "Свекла", "Майонез")
+
+    println("Введите название искомого ингредиента: ")
+    val ingredient = readln()
+    for (item in ingredientsList) {
+        if (item.equals(ingredient, ignoreCase = true)) {
+            println("Ингредиент $item в рецепте есть")
+            return
+            }
+    }
+    println("Такого ингредиента в рецепте нет")
+}
