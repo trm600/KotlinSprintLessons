69fd114d2517cd9f06db0de176944668cdfad9b9
trm600 <trm600@yahoo.com>
2026-04-21 22:42:45 +0300

    KS-9-4 added the code to the task

---
+++ src/main/kotlin/lesson_9/KS-9-4.kt
@@ -0,0 +1,11 @@
+﻿package org.example.lesson_9
+
+fun main() {
+    var ingredientsList: List<String>
+
+    do {
+        println("Введите 5 ингредиентов через \", \"(запятая с пробелом)")
+        ingredientsList = readln().split(", ")
+    } while (ingredientsList.size != 5)
+        println("Отсортированный список: ${ingredientsList.sorted().joinToString(", ")}")
+}
