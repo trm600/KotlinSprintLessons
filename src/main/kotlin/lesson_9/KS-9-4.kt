1b7b443f413f5ea0b636f2a3e9920dbaaf8a55d2
trm600 <trm600@yahoo.com>
2026-04-22 14:41:43 +0300

    KS-9-4 fix review comments

--- src/main/kotlin/lesson_9/KS-9-4.kt
+++ src/main/kotlin/lesson_9/KS-9-4.kt
@@ -1,11 +1,7 @@
 ﻿package org.example.lesson_9
 
 fun main() {
-    var ingredientsList: List<String>
-
-    do {
-        println("Введите 5 ингредиентов через \", \"(запятая с пробелом)")
-        ingredientsList = readln().split(", ")
-    } while (ingredientsList.size != 5)
-        println("Отсортированный список: ${ingredientsList.sorted().joinToString(", ")}")
+    println("Введите 5 ингредиентов через \", \"(запятая с пробелом)")
+    val ingredientsList = readln().split(", ")
+    println("Отсортированный список: ${ingredientsList.sorted()}")
 }
