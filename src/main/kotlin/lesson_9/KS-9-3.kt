909b2b048640c9f7a459d425378722486e6e5bd2
trm600 <trm600@yahoo.com>
2026-04-22 14:23:55 +0300

    KS-9-3 fix review comments

--- src/main/kotlin/lesson_9/KS-9-3.kt
+++ src/main/kotlin/lesson_9/KS-9-3.kt
@@ -1,20 +1,18 @@
 ﻿package org.example.lesson_9
 
 fun main() {
-    val productsPerServing = mutableListOf(2, 50, 15)
+    val productsPerServing = listOf(2, 50, 15)
 
     println("Введите нужное количество порций: ")
     val numberOfServings = readln().toIntOrNull()
     if (numberOfServings != null) {
-        for (index in productsPerServing.indices) {
-            productsPerServing[index] *= numberOfServings
-        }
+        val newServingList = productsPerServing.map { it * numberOfServings }
         println(
-            "На $numberOfServings порций вам понадобится: Яиц – ${productsPerServing[0]}шт., " +
-                    "молока – ${productsPerServing[1]}мл., " +
-                    "сливочного масла – ${productsPerServing[2]}гр."
+            "На $numberOfServings порций вам понадобится: Яиц – ${newServingList[0]} шт., " +
+                    "молока – ${newServingList[1]} мл., " +
+                    "сливочного масла – ${newServingList[2]} гр."
         )
     } else {
-        println("Ошибка, введите целое число.")
+        println("Ошибка, вводите целое число.")
     }
 }
