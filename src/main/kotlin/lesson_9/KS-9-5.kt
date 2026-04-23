8b9a9167013e022f1babac5e36081431d7d917bf
trm600 <trm600@yahoo.com>
2026-04-23 14:07:24 +0300

    KS-9-5 fix review comments

--- src/main/kotlin/lesson_9/KS-9-5.kt
+++ src/main/kotlin/lesson_9/KS-9-5.kt
@@ -3,31 +3,24 @@ package org.example.lesson_9
 const val INGREDIENTS_QUANTITY = 5
 
 fun main() {
-    val ingredientsList = mutableListOf<String>()
-    var counter = 1
-
     println(
         """
          ________________________
-        | Введите 5 ингредиентов:|
+        | Введите $INGREDIENTS_QUANTITY ингредиентов:|
         |________________________|
     """.trimIndent()
     )
-    do {
-        print("Введите $counter ингредиент: ")
-        val input = readln().lowercase()
-        if (input in ingredientsList) {
-            println("Такой ингредиент уже был, введите другой.")
-            continue
-        } else {
-            ingredientsList += input
-            counter++
-        }
-    } while (counter <= INGREDIENTS_QUANTITY)
-    ingredientsList.sort()
+    val ingredientsList = List(INGREDIENTS_QUANTITY) {
+        print("Введите ${it + 1} ингредиент: ")
+        readln().lowercase()
+    }
+
+    val ingredientsSet = ingredientsList
+        .sorted()
+        .toSet()
     println(
         "Итоговый список: ${
-            ingredientsList
+            ingredientsSet
                 .joinToString(", ", postfix = ".")
                 .replaceFirstChar { it.uppercase() }
         }"
