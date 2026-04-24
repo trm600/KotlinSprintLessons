3bb0615f46919f7b58fbddce521c236150336378
trm600 <trm600@yahoo.com>
2026-04-24 09:57:03 +0300

    KS-10-2 added the code to the task

---
+++ src/main/kotlin/lesson_10/KS-10-2.kt
@@ -0,0 +1,16 @@
+﻿package org.example.lesson_10
+
+fun main() {
+    println("Введите желаемый логин: ")
+    val userLogin = readln()
+    println("Введите желаемый пароль: ")
+    val userPassword = readln()
+    val isValid = checkCredentialLength(userLogin, userPassword)
+    if (!isValid) {
+        println("Логин или пароль недостаточно длинные")
+    }
+}
+
+fun checkCredentialLength(login: String, password: String): Boolean {
+    return login.length >= 4 && password.length >= 4
+}
