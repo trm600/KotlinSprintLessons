2b88320b1d16480cb63c8a7e048d76109299e626
trm600 <trm600@yahoo.com>
2026-04-28 11:28:59 +0300

    KS-11-2 added code to the task

---
+++ src/main/kotlin/lesson_11/KS-11-2.kt
@@ -0,0 +1,47 @@
+﻿package org.example.lesson_11
+
+class User2 {
+    var id: Int = 0
+    var login: String = ""
+    var password: String = ""
+    var email: String = ""
+    var bio: String = ""
+
+    fun outputInformationToConsole() {
+        println(
+            """
+    Пользователь $id:
+    Логин: $login | Пароль: $password | Почта: $email |
+    Биография: $bio
+    """.trimIndent()
+        )
+    }
+
+    fun writeBiography() {
+        println("Введите информацию о себе: ")
+        bio = readln()
+    }
+
+    fun changePassword() {
+        println("Введите текущий пароль: ")
+        if (password == readln()) {
+            println("Введите новый пароль: ")
+            password = readln()
+            println("Пароль успешно изменён!")
+        } else {
+            println("Текущий пароль неверный.")
+        }
+    }
+}
+
+fun main() {
+    val ruslan = User2()
+    ruslan.id = 1
+    ruslan.login = "ruslanCapitan"
+    ruslan.password = "1q2w3e"
+    ruslan.email = "ruslanglotov@yahoo.com"
+
+    ruslan.writeBiography()
+    ruslan.changePassword()
+    ruslan.outputInformationToConsole()
+}
