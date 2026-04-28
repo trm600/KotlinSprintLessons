1b92ef89a4d366c5cdc74a43a9d4b373b25b2be8
trm600 <trm600@yahoo.com>
2026-04-28 17:42:23 +0300

    KS-11-4 added code to the task

---
+++ src/main/kotlin/lesson_11/KS-11-4.kt
@@ -0,0 +1,22 @@
+﻿package org.example.lesson_11
+
+class Category {
+    var nameCategory: String = ""
+}
+
+class Ingredient {
+    var nameIngredient: String = ""
+    var unit: String = ""
+    var perServing: Double = 0.0
+
+}
+class Recipe {
+    var category: Category = Category()
+    var nameRecipe: String = ""
+    val ingredient: MutableList<Ingredient> = mutableListOf()
+    var servings: Int = 0
+    var isFavorite: Boolean = false
+    val cookingMethod: MutableList<String> = mutableListOf()
+}
+
+
