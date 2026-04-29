7f7623c857f69306026c79131c0c01135884e5bf
trm600 <trm600@yahoo.com>
2026-04-29 21:50:06 +0300

    KS-11-4 fix review comments...maybe

--- src/main/kotlin/lesson_11/KS-11-4.kt
+++ src/main/kotlin/lesson_11/KS-11-4.kt
@@ -1,20 +1,24 @@
 ﻿package org.example.lesson_11
 
-class Category {
-    var nameCategory: String = ""
-}
+class Category(
+    val id: Int = 0,
+    val title: String = "",
+    val nameCategory: String = "",
+    val description: String = "",
+    val imageUrl: String = "",
+    val listOfRecipes: List<String> = listOf(),
+)
 
-class Ingredient {
-    var nameIngredient: String = ""
-    var unit: String = ""
-    var perServing: Double = 0.0
-}
+class Ingredient(
+    val nameIngredient: String = "",
+    val unitOfMeasure: String = "",
+    val quantity: Int = 0,
+)
 
-class Recipe {
-    var category: Category = Category()
-    var nameRecipe: String = ""
-    val ingredient: MutableList<Ingredient> = mutableListOf()
-    var servings: Int = 0
-    var isFavorite: Boolean = false
-    val cookingMethod: MutableList<String> = mutableListOf()
-}
+class Recipe(
+    val id: Int = 0,
+    val title: String = "",
+    val ingredients: List<String> = listOf(),
+    val cookingMethod: List<String> = listOf(),
+    val imageUrl: String = "",
+)
