b93cf64b55589189c1fb67246a3c12863a56233b
trm600 <trm600@yahoo.com>
2026-05-10 08:49:22 +0300

    KS-12-2 added code to the task

---
+++ src/main/kotlin/lesson_12/KS-12-2.kt
@@ -0,0 +1,32 @@
+﻿package org.example.lesson_12
+
+class Weather2(
+    var daytimeTemperature2: Int,
+    var nighttimeTemperature2: Int,
+    var isRainfall: Boolean,
+) {
+    fun printInfo2() {
+        println(
+            """
+        Дневная температура: $daytimeTemperature2
+        Ночная температура: $nighttimeTemperature2
+        Осадки: ${
+                if (isRainfall) {
+                    "Да"
+                } else {
+                    "Нет"
+                }
+            }
+        """.trimIndent()
+        )
+    }
+}
+
+fun main() {
+    val day1 = Weather2(
+        daytimeTemperature2 = 28,
+        nighttimeTemperature2 = 21,
+        isRainfall = true,
+    )
+    day1.printInfo2()
+}
