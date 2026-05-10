4d21b74be14c3781b40e65949bc3b28029bf0d85
trm600 <trm600@yahoo.com>
2026-05-10 11:11:45 +0300

    KS-12-4 added the code to the task

---
+++ src/main/kotlin/lesson_12/KS-12-4.kt
@@ -0,0 +1,40 @@
+﻿package org.example.lesson_12
+
+import kotlin.math.roundToInt
+
+const val ABSOLUTE_ZERO: Float = 273.15f
+const val ZERO = 0
+
+class Weather4(
+    daytimeTemperature4: Int,
+    nighttimeTemperature4: Int,
+    isRainfall4: Boolean,
+) {
+    val rainfall4: String
+    val daytimeTemperature4Celsius: Int
+    val nighttimeTemperature4Celsius: Int
+
+    init {
+        require(daytimeTemperature4 >= ZERO && nighttimeTemperature4 >= ZERO) {
+            "Температура в Кельвинах не может быть отрицательной"
+        }
+        daytimeTemperature4Celsius = (daytimeTemperature4 - ABSOLUTE_ZERO).roundToInt()
+        nighttimeTemperature4Celsius = (nighttimeTemperature4 - ABSOLUTE_ZERO).roundToInt()
+        rainfall4 = if (isRainfall4) "Да" else "Нет"
+        println(
+            """
+        Дневная температура: $daytimeTemperature4Celsius
+        Ночная температура: $nighttimeTemperature4Celsius
+        Осадки: $rainfall4
+        """.trimIndent()
+        )
+    }
+}
+
+fun main() {
+    val day4 = Weather4(
+        daytimeTemperature4 = 301,
+        nighttimeTemperature4 = 290,
+        isRainfall4 = false,
+    )
+}
