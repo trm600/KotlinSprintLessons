e90ce84005b3d9baeca584cb5f6b27b7d58d2d0d
trm600 <trm600@yahoo.com>
2026-05-10 10:30:30 +0300

    KS-12-3 added the code to the task

---
+++ src/main/kotlin/lesson_12/KS-12-3.kt
@@ -0,0 +1,41 @@
+﻿package org.example.lesson_12
+
+import kotlin.math.roundToInt
+
+const val ABSOLUTE_ZERO: Float = 273.15f
+
+class Weather3(
+    daytimeTemperature3: Int,
+    nighttimeTemperature3: Int,
+    isRainfall3: Boolean,
+) {
+    val rainfall3: String
+    val daytimeTemperature3Celsius: Int
+    val nighttimeTemperature3Celsius: Int
+
+
+    init {
+        rainfall3 = if (isRainfall3) "Да" else "Нет"
+        daytimeTemperature3Celsius = (daytimeTemperature3 - ABSOLUTE_ZERO).roundToInt()
+        nighttimeTemperature3Celsius = (nighttimeTemperature3 - ABSOLUTE_ZERO).roundToInt()
+    }
+
+    fun printInfo3() {
+        println(
+            """
+        Дневная температура: $daytimeTemperature3Celsius
+        Ночная температура: $nighttimeTemperature3Celsius
+        Осадки: $rainfall3
+        """.trimIndent()
+        )
+    }
+}
+
+fun main() {
+    val day3 = Weather3(
+        daytimeTemperature3 = 301,
+        nighttimeTemperature3 = 290,
+        isRainfall3 = false,
+    )
+    day3.printInfo3()
+}
