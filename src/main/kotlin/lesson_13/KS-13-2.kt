2227b75b3bf1b74fd071d70cac02517ab379f053
trm600 <trm600@yahoo.com>
2026-05-13 10:19:36 +0300

    KS-13-2 added code to the task

---
+++ src/main/kotlin/lesson_13/KS-13-2.kt
@@ -0,0 +1,20 @@
+﻿package org.example.lesson_13
+
+class PhoneDirectory1(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?,
+) {
+    fun infoPrint() {
+        println("Имя: $name\n" + "Номер: $phoneNumber\n" + "Компания: ${company ?: "<не указано>"}")
+    }
+}
+
+fun main() {
+    val subscriber = PhoneDirectory1(
+        name = "Васёк",
+        phoneNumber = 89996123456,
+        company = null
+    )
+    subscriber.infoPrint()
+}
