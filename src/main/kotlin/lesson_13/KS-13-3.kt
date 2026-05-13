760245576d20c259b5b4e0c221b0a50016cdb170
trm600 <trm600@yahoo.com>
2026-05-13 11:40:01 +0300

    KS-13-3 added code to the task

---
+++ src/main/kotlin/lesson_13/KS-13-3.kt
@@ -0,0 +1,23 @@
+﻿package org.example.lesson_13
+
+class PhoneDirectory2(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?,
+)
+
+fun main() {
+    val phoneBook: List<PhoneDirectory2> = listOf(
+        PhoneDirectory2("Серёга", 89467362374, null),
+        PhoneDirectory2("Викторыч", 89457662374, "ООО \"Блат хата\""),
+        PhoneDirectory2("Семёныч", 89984783873, "\"null\""),
+        PhoneDirectory2("Акимыч", 89564839394, null),
+        PhoneDirectory2("Поздняк", 89456784297, "ООО \"Сервелат интернешнал\""),
+    )
+    println("Список компаний:")
+    phoneBook.forEach { contact ->
+        contact.company?.let {
+            println("Компания: $it")
+        }
+    }
+}
