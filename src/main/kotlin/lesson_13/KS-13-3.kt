ff47493a62f8a5d2d3e54368e0aef63b543c5ec4
trm600 <trm600@yahoo.com>
2026-05-14 20:04:26 +0300

    KS-13-3 fix review comments

--- src/main/kotlin/lesson_13/KS-13-3.kt
+++ src/main/kotlin/lesson_13/KS-13-3.kt
@@ -10,14 +10,10 @@ fun main() {
     val phoneBook: List<PhoneDirectory2> = listOf(
         PhoneDirectory2("Серёга", 89467362374, null),
         PhoneDirectory2("Викторыч", 89457662374, "ООО \"Блат хата\""),
-        PhoneDirectory2("Семёныч", 89984783873, "\"null\""),
+        PhoneDirectory2("Семёныч", 89984783873, "null"),
         PhoneDirectory2("Акимыч", 89564839394, null),
         PhoneDirectory2("Поздняк", 89456784297, "ООО \"Сервелат интернешнал\""),
     )
     println("Список компаний:")
-    phoneBook.forEach { contact ->
-        contact.company?.let {
-            println("Компания: $it")
-        }
-    }
+    println(phoneBook.mapNotNull { it.company }.joinToString(", ",postfix = "."))
 }
