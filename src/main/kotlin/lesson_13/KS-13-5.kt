73b71a93ac18ece9177d5a490f9f0c911bcfccef
trm600 <trm600@yahoo.com>
2026-05-15 07:42:29 +0300

    KS-13-5 added code to the task

---
+++ src/main/kotlin/lesson_13/KS-13-5.kt
@@ -0,0 +1,24 @@
+﻿package org.example.lesson_13
+
+class Contact2(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?,
+)
+
+fun main() {
+    var phoneInput: Long
+
+    println("Введите номер телефона: ")
+    try {
+        phoneInput = readln().toLong()
+    } catch (e: Exception) {
+        println("Ошибка: ${e::class.simpleName}")
+        return
+    }
+    val contact = Contact2(
+        name = "Захарыч",
+        company = "Google",
+        phoneNumber = phoneInput,
+    )
+}
