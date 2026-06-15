e7862ca9bb222266b899aa74d6161fd87184c6c7
trm600 <trm600@yahoo.com>
2026-06-15 15:09:15 +0300

    KS-15-2 added code to the task

---
+++ src/main/kotlin/lesson_15/KS-15-2.kt
@@ -0,0 +1,28 @@
+﻿package org.example.lesson_15
+
+abstract class WeatherStationStats {
+    abstract fun toDisplayString(): String
+}
+
+class Temperature(val temperature: Int) : WeatherStationStats() {
+    override fun toDisplayString(): String = "Температура: $temperature°C"
+}
+
+class PrecipitationAmount(val precipitation: Int) : WeatherStationStats() {
+    override fun toDisplayString(): String = "Осадки: $precipitation мм"
+}
+
+class WeatherServer {
+
+    fun sendMessage(data: WeatherStationStats) {
+        println(data.toDisplayString())
+    }
+}
+
+fun main() {
+    val temperature = Temperature(25)
+    val precipitation = PrecipitationAmount(10)
+    val message = WeatherServer()
+    message.sendMessage(temperature)
+    message.sendMessage(precipitation)
+}
