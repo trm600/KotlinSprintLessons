0dfe464047e82665d4f658aa03dc428bcec3ee96
trm600 <trm600@yahoo.com>
2026-06-16 20:52:19 +0300

    KS-15-4 added code to the task

---
+++ src/main/kotlin/lesson_15/KS-15-4.kt
@@ -0,0 +1,18 @@
+﻿package org.example.lesson_15
+
+abstract class Product(val name: String, val quantityInStock: Int)
+
+interface Searchable {
+    fun search() {
+        println("Выполняется поиск")
+    }
+}
+
+class MusicalInstrument(name: String, quantityInStock: Int) : Product(name, quantityInStock), Searchable
+
+class Component(name: String, quantityInStock: Int) : Product(name, quantityInStock)
+
+fun main() {
+    val instrument = MusicalInstrument("Гитара", 5)
+    instrument.search()
+}
