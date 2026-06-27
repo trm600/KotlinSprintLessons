348887fc1092ec743b4ab5749a9ec114be53b147
trm600 <trm600@yahoo.com>
2026-06-27 16:02:08 +0300

    KS-16-1 added code to the task

---
+++ src/main/kotlin/lesson_16/KS-16-1.kt
@@ -0,0 +1,16 @@
+﻿package org.example.lesson_16
+
+const val SIDES = 6
+
+class Dice {
+    private val roll: Int = (1..SIDES).random()
+
+    fun checkRoll() {
+        println("Выпало число: $roll")
+    }
+}
+
+fun main() {
+    val d = Dice()
+    d.checkRoll()
+}
