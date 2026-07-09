399c4a42ce699eeea5ebb38c7f1da1e265fa1b02
trm600 <trm600@yahoo.com>
2026-07-09 09:35:23 +0300

    KS-17-4 added code to the task

---
+++ src/main/kotlin/lesson_17/KS-17-4.kt
@@ -0,0 +1,19 @@
+﻿package org.example.lesson_17
+
+class Parcel(val idNumber: Int, currentLocation: String) {
+    var movementCounter: Int = 0
+        private set
+
+    var currentLocation = currentLocation
+        set(value) {
+            field = value
+            movementCounter++
+        }
+}
+
+fun main() {
+    val parcel = Parcel(99, "Курск")
+    println(parcel.movementCounter)
+    parcel.currentLocation = "Воронеж"
+    println(parcel.movementCounter)
+}
