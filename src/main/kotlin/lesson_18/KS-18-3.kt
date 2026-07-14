f1addb4cc44285de28ee4cf1036d5ba0b2e9c275
trm600 <trm600@yahoo.com>
2026-07-14 11:44:18 +0300

    KS-18-3 added code to the task

---
+++ src/main/kotlin/lesson_18/KS-18-3.kt
@@ -0,0 +1,45 @@
+﻿package org.example.lesson_18
+
+open class Animal(val name: String) {
+
+    open fun eat() {
+        println("$name ест")
+    }
+
+    fun sleep() {
+        println("$name -> спит")
+    }
+
+}
+
+class Fox(name: String) : Animal(name) {
+
+    override fun eat() {
+        println("$name -> ест ягоды")
+    }
+
+}
+
+class Dog(name: String) : Animal(name) {
+
+    override fun eat() {
+        println("$name -> ест кости")
+    }
+
+}
+
+class Cat(name: String) : Animal(name) {
+
+    override fun eat() {
+        println("$name -> ест рыбу")
+    }
+
+}
+
+fun main() {
+
+    val tamagochi: List<Animal> = listOf<Animal>(Cat("Мурка"), Dog("Палкан"), Fox("Алиса"))
+
+    tamagochi.forEach { it.eat() }
+
+}
