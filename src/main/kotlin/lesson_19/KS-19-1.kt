9d0e293aae29157b6502b6d09c29446af584a6c7
trm600 <trm600@yahoo.com>
2026-07-18 14:37:03 +0300

    KS-19-1 added code to the task

---
+++ src/main/kotlin/lesson_19/KS-19-1.kt
@@ -0,0 +1,19 @@
+﻿package org.example.lesson_19
+
+enum class Fish(val type: String) {
+
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок")
+
+}
+
+fun main() {
+
+    println(
+        "Можно добавить в аквариум следующих рыб: " +
+                Fish.entries.joinToString(", ", postfix = ".") { it.type }
+    )
+
+}
