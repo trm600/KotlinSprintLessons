699b54f36379e0f47dde2bcc2e931ea4e61f69d7
trm600 <trm600@yahoo.com>
2026-07-19 07:05:52 +0300

    KS-19-3 added code to the task

---
+++ src/main/kotlin/lesson_19/KS-19-3.kt
@@ -0,0 +1,21 @@
+﻿package org.example.lesson_19
+
+class Spaceship(val name: String) {
+
+    fun takeOff() {
+        //TODO Нужна информация о дополнительной логике
+    }
+
+    fun landing() {
+        TODO()
+    }
+
+    fun shootAsteroid() {}
+
+}
+
+fun main() {
+    val ship = Spaceship("Лодочка")
+    ship.takeOff()
+    ship.landing()
+}
