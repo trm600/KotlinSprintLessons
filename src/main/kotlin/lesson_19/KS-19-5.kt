d46211d7655bf46e9288167c171a9e8a33e192a7
trm600 <trm600@yahoo.com>
2026-07-21 11:21:32 +0300

    KS-19-5 added code to the task

---
+++ src/main/kotlin/lesson_19/KS-19-5.kt
@@ -0,0 +1,62 @@
+﻿package org.example.lesson_19
+
+enum class Gender(val rusName: String) {
+
+    MALE("мужской"),
+    FEMALE("женский");
+
+}
+
+class People private constructor(val name: String, val gender: Gender) {
+
+    companion object {
+
+        fun addPerson(peopleName: String, peopleGender: String): People {
+
+            require(peopleName.isNotBlank()) { "Имя не может быть пустым! Повторите ввод!\n" }
+            require(peopleGender.isNotBlank()) { "Пол не может быть пустым! Повторите ввод!\n" }
+            require(peopleGender == "муж" || peopleGender == "жен") {
+                "Введен некорректный пол!" +
+                        "Повторите ввод!\n"
+            }
+
+            fun checkGender(): Gender {
+                return if (peopleGender == "муж") {
+                    Gender.MALE
+                } else {
+                    Gender.FEMALE
+                }
+            }
+
+            return People(name = peopleName, gender = checkGender())
+
+        }
+    }
+
+}
+
+fun main() {
+
+    val cardIndex = mutableListOf<People>()
+    println("Доступны мужской и женский пол. Пол вводить в формате:\nмуж - мужской\nжен - женский\n")
+
+    while (cardIndex.size < 5) {
+        println("Введите имя человека: ")
+        val name = readln().replaceFirstChar { it.uppercase() }.trim()
+        println("Введите пол человека: ")
+        val gender = readln().lowercase().trim()
+        println()
+
+        try {
+            cardIndex.add(People.addPerson(name, gender))
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+
+    }
+
+    cardIndex.forEach { people ->
+        println("${people.name} пол ${people.gender.rusName}.")
+    }
+
+}
