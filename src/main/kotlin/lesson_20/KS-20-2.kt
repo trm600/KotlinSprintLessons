c29bcb89dc8855b4a3de9f96953994d9b137ea75
trm600 <trm600@yahoo.com>
2026-07-21 15:21:20 +0300

    KS-20-2 added code to the task

---
+++ src/main/kotlin/lesson_20/KS-20-2.kt
@@ -0,0 +1,17 @@
+﻿package org.example.lesson_20
+
+class Character(val name: String, var currentHealth: Int, val maxHealth: Int)
+
+val pickUpHealingPotion: (Character) -> Unit = { character ->
+    println("${character.name} захавал хилку!")
+    character.currentHealth = character.maxHealth }
+
+fun main() {
+
+    val character1 = Character(name = "Alice", currentHealth = 20, maxHealth = 100)
+
+    println("Текущее здоровье: ${character1.currentHealth}")
+    pickUpHealingPotion(character1)
+    println("Текущее здоровье: ${character1.currentHealth}")
+
+}
