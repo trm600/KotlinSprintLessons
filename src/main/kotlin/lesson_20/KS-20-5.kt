9db580fa8eb6fbeea5a06707d9b9a6d1ae8d9af2
trm600 <trm600@yahoo.com>
2026-07-22 13:28:45 +0300

    KS-20-5 added code to the task

---
+++ src/main/kotlin/lesson_20/KS-20-5.kt
@@ -0,0 +1,37 @@
+﻿package org.example.lesson_20
+
+class Robot {
+
+    val phrases = listOf(
+        "Раз два раз два, настройка звука",
+        "Любишь кататься, люби себе дальше",
+        "Один в сексе не воин",
+        "Чёткая фраза",
+        "На посошок и на горшок"
+    )
+    var modify: ((String) -> String)? = null
+
+    fun setModifier(modifier: (String) -> String) {
+        modify = modifier
+    }
+
+    fun say() {
+
+        val phrase = phrases.random()
+        val result = modify?.invoke(phrase) ?: phrase
+
+        println(result)
+
+    }
+
+}
+
+fun main() {
+
+    val robot1 = Robot()
+
+    robot1.say()
+    robot1.setModifier { phrase -> phrase.reversed().lowercase().replaceFirstChar { it.uppercase() } }
+    robot1.say()
+
+}
