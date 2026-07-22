77061dc20aed5f3ce1acd61e3b492b9d23311700
trm600 <trm600@yahoo.com>
2026-07-22 09:50:58 +0300

    KS-20-4 added code to the task

---
+++ src/main/kotlin/lesson_20/KS-20-4.kt
@@ -0,0 +1,14 @@
+﻿package org.example.lesson_20
+
+fun main() {
+
+    val elements = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
+    val lambdaElements: List<() -> Unit> = elements.map { { println("Нажат элемент $it") } }
+
+    for ((index, element) in lambdaElements.withIndex()) {
+        if ((index + 1) % 2 == 0) {
+            element()
+        }
+    }
+
+}
