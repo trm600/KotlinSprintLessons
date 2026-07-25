5125330e3b4dcb663d7c9d000603d4db5c904ace
trm600 <trm600@yahoo.com>
2026-07-25 12:28:02 +0300

    KS-21-4 added code to the task

---
+++ src/main/kotlin/lesson_21/KS-21-4.kt
@@ -0,0 +1,24 @@
+﻿package org.example.lesson_21
+
+import java.io.File
+
+fun File.writeFormatedWord(wordToWrite: String) {
+
+    val existingContent = if (exists()) readText() else ""
+
+    writeText("${wordToWrite.lowercase()}\n$existingContent")
+    println("Слово $wordToWrite записано в файл $name \n")
+
+}
+
+fun main() {
+
+    val file = File("EnglishWords.txt")
+
+    file.writeFormatedWord("Move")
+    file.writeFormatedWord("Girl")
+    file.writeFormatedWord("Rice")
+
+    println(file.readText())
+
+}
