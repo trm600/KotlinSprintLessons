da4b25c500ad851b2a218a3a7ac1d52d507723d0
trm600 <trm600@yahoo.com>
2026-07-25 11:50:20 +0300

    KS-21-3 added code to the task

---
+++ src/main/kotlin/lesson_21/KS-21-3.kt
@@ -0,0 +1,15 @@
+﻿package org.example.lesson_21
+
+class Player(val name: String, var currentHealth: Int, val maxHealth: Int)
+
+fun Player.isHealthy() : Boolean = currentHealth == maxHealth
+
+fun main() {
+
+    val player1 = Player(name = "Alice", currentHealth = 20, maxHealth = 100)
+    val player2 = Player(name = "Frimen", currentHealth = 100, maxHealth = 100)
+
+    println(player1.isHealthy())
+    println(player2.isHealthy())
+
+}
