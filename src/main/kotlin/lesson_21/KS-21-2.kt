e05278dc68cdeed2f139bacad117318fc47984f5
trm600 <trm600@yahoo.com>
2026-07-25 11:26:16 +0300

    KS-21-2 added code to the task

---
+++ src/main/kotlin/lesson_21/KS-21-2.kt
@@ -0,0 +1,8 @@
+﻿package org.example.lesson_21
+
+fun List<Int>.evenNumbersSum(): Int = filter { it % 2 == 0 }.sum()
+
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(list.evenNumbersSum())
+}
