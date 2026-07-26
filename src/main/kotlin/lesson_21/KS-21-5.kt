2933864f6173765ade9aa1aa66571f49ed418fa6
trm600 <trm600@yahoo.com>
2026-07-26 20:56:01 +0300

    KS-21-5 fix review comments

--- src/main/kotlin/lesson_21/KS-21-5.kt
+++ src/main/kotlin/lesson_21/KS-21-5.kt
@@ -1,22 +1,18 @@
 ﻿package org.example.lesson_21
 
-fun Map<String, Int>.maxCategory() : String {
-
-    val maxPoints = maxByOrNull { it.value }
-    return maxPoints?.key ?: ""
-
-}
+fun Map<String, Int>.maxCategory(): String? = maxByOrNull { it.value }?.key
 
 fun main() {
 
     val scores = mapOf(
+
         "Cила" to 85,
         "Ловкость" to 92,
         "Скрытность" to 78,
         "Выносливость" to 95,
         "Красноречие" to 95,
 
-    )
+        )
 
     println(scores.maxCategory())
 
